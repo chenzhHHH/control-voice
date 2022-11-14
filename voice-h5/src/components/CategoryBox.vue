16d3956469a9945bb7cd808a8b513ab1a2014a1d
chenzh <502804924@qq.com>
2022-11-14 09:51:46 +0800

    change home page button style

---
+++ voice-h5/src/components/CategoryBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="containerBox" :style="{backgroundColor: activeBackgroundColor}">
+        <div>雷达基础理论 {{testColor}}</div>
+        <div style="margin-left:0.5rem"><van-image width="3rem" height="3rem" it="contain" :src="iconSrc"/></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        props: {
+            iconSrc: String,
+            activeBackgroundColor: String
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+    .containerBox {
+        width: 10.5rem;
+        height: 5.5rem;
+        border-radius: 0.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1rem;
+        color: white;
+    }
+</style>
--- voice-h5/src/views/HomeView.vue
+++ voice-h5/src/views/HomeView.vue
@@ -18,106 +18,54 @@
       <van-col span="4" offset="15" style="font-size:10px;">更多</van-col>
   </van-row>
 
-  <van-row>
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0" @click='goTo()'>
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">雷达基础理论</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
+  <div>
+    <van-row justify="center" style="margin: 1rem 0 0 0">
+      <van-col span="10" >
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#E74C3C"></category-box>
+      </van-col>
 
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">机场管制</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
+      <van-col span="10" offset="1">
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#AF7AC5"></category-box>
+      </van-col>
+    </van-row>
 
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">进近管制</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
-  </van-row>
+    <van-row justify="center" style="margin: 1rem 0 0 0">
+      <van-col span="10" >
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#85C1E9"></category-box>
+      </van-col>
 
-  <van-row>
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">区域管制</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
-    
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">特情</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
+      <van-col span="10" offset="1">
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#F5B041"></category-box>
+      </van-col>
+    </van-row>
 
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/learnCenter.png')"/>
-        <div>
-          <span style="font-size: 13px">管制英语</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
-  </van-row>
+    <van-row justify="center" style="margin: 1rem 0 0 0">
+      <van-col span="10" >
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#C0392B"></category-box>
+      </van-col>
 
-  <van-row>
-    <van-col span="8" style="text-align: center; border: 0.5px solid #c0c0c0">
-      <div style="margin: 20px 0 10px 0">
-        <van-image width="50px" height="50px" it="contain" :src="require('../assets/add.png')"/>
-        <div>
-          <span style="font-size: 13px">雷达基础理论</span>
-        </div>
-        <div>
-          <span style="color: #c0c0c0">自定义添加</span>
-        </div>
-      </div>
-    </van-col>
-  </van-row>
-
+      <van-col span="10" offset="1">
+        <category-box :icon-src="require('../assets/trophy.png')" active-background-color="#229954"></category-box>
+      </van-col>
+    </van-row>
+  </div>
 </template>
 
 <script>
+  import CategoryBox from '@/components/CategoryBox.vue'
   import { Swipe, SwipeItem, Search } from 'vant';
 
   export default {
     components: {
+      CategoryBox,
       [Swipe.name]: Swipe,
       [SwipeItem.name]: SwipeItem,
       [Search.name]: Search
+    },
+    data() {
+      return {
+        
+      }
     },
     methods: {
       goTo() {
