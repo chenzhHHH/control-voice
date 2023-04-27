29fce4f291e4ec1e4cd26f9d4a1e03ed7b30f565
chenzh <502804924@qq.com>
2023-04-27 11:04:03 +0800

    delete useless

--- voice/src/main/java/com/kfs/voice/mapper/ExamQuestionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kfs.voice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kfs.voice.entity.ExamQuestion;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ExamQuestionMapper extends BaseMapper<ExamQuestion> {
-}
