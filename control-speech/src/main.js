8d456a3940b97e5bd6d1e3d180f6dc678475d081
chenzh <502804924@qq.com>
2023-09-06 15:06:30 +0800

    control-speech - add element icon

--- control-speech/src/main.js
+++ control-speech/src/main.js
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App)
-.use(ElementPlus)
-.use(store)
-.use(router)
-.mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+    .use(store)
+    .use(router)
+    .mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
